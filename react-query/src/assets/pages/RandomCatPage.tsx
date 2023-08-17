34e884e714df06957c7fa531741c6dc6a17cb32d
christinen22 <christine.elin.louise@gmail.com>
2023-08-17 11:33:57 +0200

    .

--- react-query/src/assets/pages/RandomCatPage.tsx
+++ react-query/src/assets/pages/RandomCatPage.tsx
@@ -1,4 +1,4 @@
-import { getCatBreed } from "../../services/api"; // Assuming you have this function
+import { getCatBreed } from "../../services/api";
 import { useQuery } from "@tanstack/react-query";
 import Loading from "../components/Loading";
 import Alert from "react-bootstrap/Alert";
